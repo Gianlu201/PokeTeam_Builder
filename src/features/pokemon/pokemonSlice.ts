226c3e1619968371cb705b61ffab4e213561c4d0
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-06-04 17:27:55 +0200

    redux setted

---
+++ src/features/pokemon/pokemonSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { Pokemon } from '../../types/pokemon';
+
+interface PokemonState {
+  list: Pokemon[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: PokemonState = {
+  list: [],
+  loading: false,
+  error: null,
+};
+
+const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState,
+  reducers: {
+    setReduxPokemonList(state, action: PayloadAction<Pokemon[]>) {
+      state.list = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    setLoading(state, action: PayloadAction<boolean>) {
+      state.loading = action.payload;
+    },
+    setError(state, action: PayloadAction<string | null>) {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setReduxPokemonList, setLoading, setError } =
+  pokemonSlice.actions;
+export default pokemonSlice.reducer;
--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from 'react';
 import type { Pokemon } from '../types/pokemon';
 import PokemonBox from '../components/PokemonBox';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { setReduxPokemonList } from '../features/pokemon/pokemonSlice';
+import { Search } from 'lucide-react';
 
 const ExplorePage = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const reduxPokemonList = useAppSelector((state) => state.pokemon.list);
+
+  const dispatch = useAppDispatch();
 
   const getAllPokemons = async () => {
     try {
@@ -65,6 +73,7 @@ const ExplorePage = () => {
       if (response.ok) {
         const data = await response.json();
         console.info(data);
+        dispatch(setReduxPokemonList(data.data.pokemon_v2_pokemon));
         setPokemonList(data.data.pokemon_v2_pokemon);
       } else {
         throw new Error('Error in fetching datas');
@@ -74,14 +83,43 @@ const ExplorePage = () => {
     }
   };
 
+  const updateFilteredList = () => {
+    setPokemonList(
+      reduxPokemonList.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  };
+
   useEffect(() => {
-    if (pokemonList.length === 0) {
+    if (reduxPokemonList.length === 0) {
       getAllPokemons();
+    } else {
+      setPokemonList(reduxPokemonList);
     }
   }, []);
 
+  useEffect(() => {
+    updateFilteredList();
+  }, [searchQuery]);
+
   return (
     <div className='max-w-7xl mx-auto'>
+      <div className='flex justify-between items-center my-4'>
+        <div className='grow relative border border-gray-400/30 rounded-xl p-3'>
+          <input
+            type='text'
+            placeholder='Search for Pokémon by name..'
+            className='w-full ms-10 focus:outline-0'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <Search className='absolute top-1/2 start-4 -translate-y-1/2' />
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+
       {pokemonList.length > 0 && <PokemonBox pokemonList={pokemonList} />}
     </div>
   );
