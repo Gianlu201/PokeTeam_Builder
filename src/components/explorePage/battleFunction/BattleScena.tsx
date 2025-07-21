c5b079e9520c8df70891977bfa1da3ca434f1c67
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-07-21 17:14:31 +0200

    fight environment wip

--- src/components/explorePage/battleFunction/BattleScena.tsx
+++ src/components/explorePage/battleFunction/BattleScena.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import type { Pokemon } from '../../../types/APITypes';
 import { teamPreparation } from '../../../utils/mainUtils';
 import type { PokeTeam } from '../../../types/myTypes';
+import PokeScooter from './pokeScooter';
 
 interface Props {
   mySelectedTeam: PokeTeam | undefined;
@@ -54,46 +55,46 @@ const BattleScena = ({ mySelectedTeam, enemySelectedTeam }: Props) => {
 
   return (
     <>
-      <div>
-        {myTeam.length > 0 && (
-          <>
-            {myTeam.map((pokemon) => (
-              <>
-                <div>
-                  {pokemon.name} {pokemon.pokemon_v2_pokemonmoves?.length}
-                </div>
-                <ul>
-                  {pokemon.pokemon_v2_pokemonmoves &&
-                    pokemon.pokemon_v2_pokemonmoves.map((move) => (
-                      <li className='text-red-700'>
-                        {move.pokemon_v2_move.name}
-                      </li>
-                    ))}
-                </ul>
-              </>
-            ))}
-          </>
-        )}
-      </div>
+      {myTeam.length > 0 && enemyTeam.length > 0 && (
+        <div>
+          <div
+            className='max-w-[600px] max-h-[310px] mx-auto px-6 py-2 bg-no-repeat bg-contain'
+            style={{
+              backgroundImage: `url('public/images/battleBackground.jpg')`,
+            }}
+          >
+            {/* enemy team */}
+            <div className='grid grid-cols-12 items-start'>
+              <div className='col-span-5'>
+                <PokeScooter pokeName={enemyTeam[0].name} />
+              </div>
+              <div className='col-span-7 flex justify-center items-center'>
+                <img
+                  className='w-[150px] h-[150px] relative top-14'
+                  src={
+                    enemyTeam[0].pokemon_v2_pokemonsprites[0].front_sprite ?? ''
+                  }
+                  alt={enemyTeam[0].name}
+                />
+              </div>
+            </div>
 
-      <div>
-        {enemyTeam.length > 0 && (
-          <>
-            {enemyTeam.map((pokemon) => (
-              <>
-                <div>{pokemon.name}</div>
-                <ul>
-                  {pokemon.pokemon_v2_pokemonmoves?.map((move) => (
-                    <li>
-                      {move.pokemon_v2_move.name} - {move.pokemon_v2_move.power}
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ))}
-          </>
-        )}
-      </div>
+            {/* my team */}
+            <div className='grid grid-cols-12 items-start'>
+              <div className='col-span-7 flex justify-center items-center'>
+                <img
+                  className='w-[150px] h-[150px] relative bottom-6'
+                  src={myTeam[0].pokemon_v2_pokemonsprites[0].back_sprite ?? ''}
+                  alt={myTeam[0].name}
+                />
+              </div>
+              <div className='col-span-5'>
+                <PokeScooter pokeName={myTeam[0].name} />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
---
+++ src/components/explorePage/battleFunction/PokeScooter.tsx
@@ -0,0 +1,30 @@
+interface Props {
+  pokeName: string;
+}
+
+const PokeScooter = ({ pokeName }: Props) => {
+  return (
+    <>
+      <div className='grid grid-cols-4 gap-2 bg-gray-300 mt-10 px-3 py-1'>
+        <div className='col-span-3 flex flex-col gap-2'>
+          <h4 className='font-bold capitalize'>{pokeName}</h4>
+          <div className='w-full h-[8px] bg-green-500 border border-gray-800/60 rounded-full mb-2'></div>
+        </div>
+        <div className='col-span-1'>
+          <span className='font-bold text-xs'>Lv. 50</span>
+        </div>
+      </div>
+
+      <div className='flex gap-2 justify-start items-center mt-1'>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+        <div className='w-5 h-5 rounded-full border border-gray-800/70 bg-gray-500/70'></div>
+      </div>
+    </>
+  );
+};
+
+export default PokeScooter;
