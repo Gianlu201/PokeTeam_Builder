96717d4f990d645b3664b2c99fe3fa01f4216f5e
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-08-04 15:58:21 +0200

    moves box colored by move type

--- src/components/explorePage/battleFunction/PokemonMove.tsx
+++ src/components/explorePage/battleFunction/PokemonMove.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import type { Pokemon_v2_move } from '../../../types/APITypes';
+import { typeColors } from '../../../utils/typeColors';
 
 interface Props {
   move: Pokemon_v2_move;
@@ -15,7 +16,10 @@ const PokemonMove = ({ move, setChosenMove }: Props) => {
 
   return (
     <div
-      className='bg-gray-300 border border-gray-600/50 rounded-md px-2 py-1 text-sm font-medium'
+      className='bg-gray-300 border border-gray-600/50 rounded-md px-2 py-1 text-sm font-medium text-white'
+      style={{
+        backgroundColor: typeColors[move.pokemon_v2_type.name],
+      }}
       onClick={() => {
         if (currentPP > 0) {
           setCurrentPP((state) => state - 1);
